29ecebf1d6ba415e7f36880e004ea83e0d628f27
Klaas Freitag <freitag@owncloud.com>
2015-03-14 11:18:55 +0100

    Add ability to do dark pause

--- sketchbook/visualize/visualize.ino
+++ sketchbook/visualize/visualize.ino
@@ -22,24 +22,28 @@
 
 #define LEDCNT 3
 #define LED_ON_VAL 255
-#define LED_OFF_VAL 20
+#define LED_OFF_VAL 5
 
 #define STATE_PLAY  1
 #define STATE_PAUSE 2
 #define STATE_STOP  3
 #define STATE_UNKNOWN 0
 
-#define DURATION 800
+#define PAUSE 2000
+
+#define DURATION 1000
 
 int _state = 0;
 int _current_led;
 boolean _state_change = true;
 
 LEDFader _leds[LEDCNT] = {
-  LEDFader(3),
   LEDFader(5),
-  LEDFader(6)
+  LEDFader(6),
+  LEDFader(9)
 };
+
+unsigned long _pauseStart[LEDCNT];
 
 void setup()
 {
@@ -53,8 +57,9 @@ void setup()
   for( int i = 0; i < LEDCNT; i++ ) {
     _leds[i].set_value(LED_OFF_VAL);
     _leds[i].fade(LED_ON_VAL, DURATION);
+    _pauseStart[i] = 0;
   }
-  _state = STATE_PLAY;
+  _state = STATE_UNKNOWN;
 }
 
 void loop()
@@ -85,11 +90,33 @@ void visualize_state()
       if( led->get_value() == LED_ON_VAL ) {
         led->fade(LED_OFF_VAL, DURATION);
       } else {
-        // LED has faded out. Start the next one
-        _current_led++;
-        if( _current_led > LEDCNT-1 ) _current_led = 0;
-        led = &_leds[_current_led];
-        led->fade(LED_ON_VAL, DURATION);
+        bool pause = true;
+        // LED has faded out. 
+        if( _pauseStart[_current_led] == 0 ) {
+           // this is the first loop after the fade ended.
+          _pauseStart[_current_led] = millis(); 
+        } else {
+          unsigned long now = millis();
+          if( _pauseStart[_current_led] > now ) {
+            // that means that the millis-counter wrapped around
+          } else {
+            if( now - _pauseStart[_current_led] < PAUSE ) {
+               // still pause
+               pause = true;
+            } else {
+              // pause has ended.
+              pause = false;
+              _pauseStart[_current_led] = 0;
+            }
+          }
+        }
+        // Start the next one
+        if( ! pause ) {
+            _current_led++;
+            if( _current_led > LEDCNT-1 ) _current_led = 0;
+            led = &_leds[_current_led];
+            led->fade(LED_ON_VAL, DURATION);
+        }
       }
     } else {
       // LED is fading in or out, all nice.
